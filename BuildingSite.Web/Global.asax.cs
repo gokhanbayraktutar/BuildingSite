75d303baf68a1127f089009dc09446d07007830d
Gokhan <bayraktutargokhan@gmail.com>
2020-12-30 20:10:34 +0300

    Mapper metodu eklendi..

--- BuildingSite.Web/Global.asax.cs
+++ BuildingSite.Web/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using BuildingSite.Data.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +18,12 @@ namespace BuildingSite.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Mapping Initialized
+            Mapper.Initialize(cfg =>
+            {
+                MapUtility.ConfigureMapping(cfg);
+            });
         }
     }
 }
