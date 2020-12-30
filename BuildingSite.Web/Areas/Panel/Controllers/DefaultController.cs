056d9030711f301aea4a1832f9e728e5ac2ba947
Gokhan <bayraktutargokhan@gmail.com>
2020-12-31 00:58:28 +0300

    DefaultController eklendi.

---
+++ BuildingSite.Web/Areas/Panel/Controllers/DefaultController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildingSite.Web.Areas.Panel.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Panel/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Panel/Default/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Panel/Default/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Panel/Default/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Panel/Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Panel/Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Panel/Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Panel/Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
