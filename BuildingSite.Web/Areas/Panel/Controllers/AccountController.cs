7d89ed27f429b09e9adda7aa81c91f5c0c0c38d1
Gokhan <bayraktutargokhan@gmail.com>
2021-01-01 14:48:30 +0300

    Login post ve logout eklendi.

--- BuildingSite.Web/Areas/Panel/Controllers/AccountController.cs
+++ BuildingSite.Web/Areas/Panel/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
 ﻿using BuildingSite.Contracts.IRepository;
+using BuildingSite.Model.EntityModels;
+using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace BuildingSite.Web.Areas.Panel.Controllers
 {
@@ -15,6 +18,35 @@ namespace BuildingSite.Web.Areas.Panel.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AdminModel credentials)
+        {
+            AdminModel user = _adminService.GetAll().FirstOrDefault(x => x.UserName == credentials.UserName && x.Password == credentials.Password);
+            if (user != null)
+            {
+                string cookie = user.UserName;
+                FormsAuthentication.SetAuthCookie(cookie, true);
+                Session["FullName"] = user.UserName;
+                Session["ID"] = user.Id;
+                return Redirect("/Panel/Default/Index");
+            }
+            else
+            {
+                ViewBag.error = "Kullanıcı Adı Veya Şifre Hatalı!";
+                return View(credentials);
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Abandon();
+            FormsAuthentication.SignOut();
+            Session["FullName"] = null;
+            Session["Picture"] = null;
+            Session["ID"] = null;
+            return Redirect("/Panel/Account/Login");
         }
     }
 }
