47ef06a95a4eaef312eb7bb787e232d02d04c303
Gokhan <bayraktutargokhan@gmail.com>
2021-01-01 14:19:55 +0300

    Login ekranı eklendi.

---
+++ BuildingSite.Web/Areas/Panel/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using BuildingSite.Contracts.IRepository;
+using System.Web.Mvc;
+
+namespace BuildingSite.Web.Areas.Panel.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAdminService _adminService;
+
+        public AccountController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
