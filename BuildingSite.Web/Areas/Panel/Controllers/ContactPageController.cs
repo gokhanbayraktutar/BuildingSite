3c31bce1686e1c612315ef52b39c5a4f9ce3881f
Gokhan <bayraktutargokhan@gmail.com>
2021-01-06 23:35:17 +0300

    edit eklendi

---
+++ BuildingSite.Web/Areas/Panel/Controllers/ContactPageController.cs
@@ -0,0 +1,45 @@
+﻿using BuildingSite.Contracts.IRepository;
+using BuildingSite.Model.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildingSite.Web.Areas.Panel.Controllers
+{
+    public class ContactPageController : Controller
+    {
+        private readonly IContactPageService _contactPageService;
+
+        public ContactPageController(IContactPageService contactPageService)
+        {
+            _contactPageService = contactPageService;
+        }
+
+        // GET: Panel/ContactPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ContactPageModel model = _contactPageService.GetById(id);
+
+            return View(model);
+
+        }
+
+
+
+        public ActionResult Edit(ContactPageModel contactPageModel)
+        {
+
+             _contactPageService.Update(contactPageModel);
+
+            return RedirectToAction("Edit");
+        }
+    }
+}
