5fcaada6b8aaa4de3fe7a886986afbbc23b3698c
Gokhan <bayraktutargokhan@gmail.com>
2021-01-06 18:57:25 +0300

    AboutUsPageService eklendi.

---
+++ BuildingSite.Contracts/IRepository/IAboutUsPageService.cs
@@ -0,0 +1,10 @@
+﻿using BuildingSite.Contracts.IRepository.Base;
+using BuildingSite.Data.Entities;
+using BuildingSite.Model.EntityModels;
+
+namespace BuildingSite.Contracts.IRepository
+{
+    public interface IAboutUsPageService : IService<AboutUsPage, AboutUsPageModel>
+    {
+    }
+}
---
+++ BuildingSite.Services/Repository/AboutUsPageService.cs
@@ -0,0 +1,23 @@
+﻿using BuildingSite.Contracts.IRepository;
+using BuildingSite.Data.Context;
+using BuildingSite.Data.Entities;
+using BuildingSite.Model.EntityModels;
+using BuildingSite.Services.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingSite.Services.Repository
+{
+    public class AboutUsPageService : Service<AboutUsPage, AboutUsPageModel>, IAboutUsPageService
+    {
+        private DataContext _context;
+
+        public AboutUsPageService(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
