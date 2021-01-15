1941aa84abfdd164300a822c38143013b7f29907
Gokhan <bayraktutargokhan@gmail.com>
2021-01-15 22:42:32 +0300

    NewsService eklendi.

---
+++ BuildingSite.Services/Repository/NewsService.cs
@@ -0,0 +1,23 @@
+﻿using BuildingSite.Contracts.IRepository;
+using BuildingSite.Data.Context;
+using BuildingSite.Data.Entities;
+using BuildingSite.Model.EntityModels;
+using BuildingSite.Services.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingSite.Services.Repository
+{
+    public class NewsService:Service<News, NewsModel>,INewsService
+    {
+        private DataContext _context;
+
+    public NewsService(DataContext context) : base(context)
+    {
+        _context = context;
+    }
+}
+}
