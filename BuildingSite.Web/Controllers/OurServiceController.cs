32a3e5aa5ae657de67e56f7fae09b09dbef0f94d
Gokhan <bayraktutargokhan@gmail.com>
2021-01-16 18:39:18 +0300

    OurServiceController ve Index sayfası eklemeler yapıldı.

---
+++ BuildingSite.Web/Controllers/OurServiceController.cs
@@ -0,0 +1,62 @@
+﻿using BuildingSite.Contracts.IRepository;
+using BuildingSite.Model.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuildingSite.Web.Controllers
+{
+    public class OurServiceController : Controller
+    {
+        private readonly ISiteConstantService _siteConstantService;
+
+        private readonly IOurServiceService _ourServiceService;
+
+        private readonly IProjectCategoryService _projectCategoryService;
+
+        private readonly IProjectService _projectService;
+
+        public OurServiceController(ISiteConstantService siteConstantService, IOurServiceService ourServiceService, IProjectCategoryService projectCategoryService, IProjectService projectService)
+        {
+            _siteConstantService = siteConstantService;
+
+            _ourServiceService = ourServiceService;
+
+            _projectCategoryService = projectCategoryService;
+
+            _projectService = projectService;
+        }
+
+        // GET: OurService
+        public ActionResult Index()
+        {
+            ViewModel model = new ViewModel();
+
+            model.SiteConstantModel = _siteConstantService.GetById(6);
+
+            model.ourServiceModels = _ourServiceService.GetAll().Where(x => x.Active == true).ToList();
+
+            model.ProjectCategoryModels = _projectCategoryService.GetAll().Where(x => x.Active == true).ToList();
+
+            model.ProjectModels = _projectService.GetAll().Where(x => x.Active == true).ToList();
+
+            return View(model);
+        }
+
+        public ActionResult Detail()
+        {
+            ViewModel model = new ViewModel();
+
+            model.ourServiceModels = _ourServiceService.GetAll().Where(x => x.Active == true).ToList();
+
+            model.ProjectCategoryModels = _projectCategoryService.GetAll().Where(x => x.Active == true).ToList();
+
+            model.ProjectModels = _projectService.GetAll().Where(x => x.Active == true).ToList();
+
+            return View(model);
+        }
+
+    }
+}
